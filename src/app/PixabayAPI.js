e4bb7fd6e169650d7473697ac958c34a5bad26ee
Maryna Bludenko <marineblu@icloud.com>
2023-09-12 15:33:09 +0100

    updated

---
+++ src/app/PixabayAPI.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://pixabay.com/api/";
+const API_KEY = "39384633-c501652c7972b3b9d1bf511e1";
+
+
+export class PixabayAPI {
+  #page = 1;
+  #per_page = 40;
+  #query = '';
+  #totalPhotos = 0;
+
+  async getPhotos() {
+    const params = {
+      page: this.#page,
+      q: this.#query,
+      per_page: this.#per_page,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    };
+
+    const urlAXIOS = `?key=${API_KEY}`;
+    const { data } = await axios.get(urlAXIOS, { params });
+    return data;
+  }
+
+  get query() {
+    return this.#query;
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+
+  incrementPage() {
+    this.#page += 1;
+  }
+
+  resetPage() {
+    this.#page = 1;
+  }
+
+  setTotal(total) {
+    this.#totalPhotos = total;
+  }
+
+  hasMorePhotos() {
+    return this.#page < Math.ceil(this.#totalPhotos / this.#per_page);
+  }
+}
